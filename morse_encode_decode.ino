41c51c0674345bc373a783b450cd17452fe082f1
kodera2t <kodera2t@yahoo.co.jp>
2015-02-16 08:54:09 +0900

    minor bug fix

--- morse_encode_decode.ino
+++ morse_encode_decode.ino
@@ -147,8 +147,9 @@ if(c <= 128){
 			screenMem[21 + xg] = screenMem[42 + xg];		
 			screenMem[42 + xg] = screenMem[63 + xg];
 			screenMem[63 + xg] = screenMem[84 + xg];
-			screenMem[84 + xg] = screenMem[125 + xg];		
-			screenMem[125 + xg] = screenMem[146 + xg];
+			screenMem[84 + xg] = screenMem[105 + xg];
+			screenMem[105 + xg] = screenMem[126 + xg];		
+			screenMem[126 + xg] = screenMem[147 + xg];
 
 
 
